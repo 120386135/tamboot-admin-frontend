8e6b4baf048515f00f207d7a51d41a9a279007fa
chensheng <742611497@qq.com>
2019-07-15 18:58:15 +0800

    refactor PageView

--- src/components/PageView/index.js
+++ src/components/PageView/index.js
@@ -1,13 +1,11 @@
 import React, { PureComponent } from 'react';
-import { Row, Col, Form, Input, Button, Card } from 'antd';
+import { Input, Button, Card } from 'antd';
 import StandardTable from '@/components/StandardTable';
+import SearchForm from '@/components/SearchForm';
 
 import styles from '@/utils/custom.less';
 
-const FormItem = Form.Item;
-
 /* eslint react/no-multi-comp:0 */
-@Form.create()
 class PageView extends PureComponent {
   static defaultProps = {
     selectable: false,
@@ -15,7 +13,7 @@ class PageView extends PureComponent {
     searchFormItems: [],
     searchFormItemLayout: { md: 6, sm: 24 },
     searchFormRowGutter: { md: 8, lg: 24, xl: 48 },
-    columnSpan: 4,
+    searchFormColumnSpan: 4,
     operatorComponents: [],
     columns: [],
     pageData: {
@@ -34,24 +32,16 @@ class PageView extends PureComponent {
     super(props);
 
     this.state = {
-      searchFormValues: {},
       selectedRows: [],
-      showAdvancedSearchForm: false
     };
   }
 
   componentDidMount() {
-    this.props.bindSearch(this.doSearch);
+    this.props.bindSearch(this.handleSearch);
     this.props.bindGetSelectedRows(this.doGetSelectedRows);
 
+    this.doSearch();
     const { dispatch, pageEffectType, defaultPageNum, defaultPageSize } = this.props;
-    dispatch({
-      type: pageEffectType,
-      payload: {
-        pageNum: defaultPageNum,
-        pageSize: defaultPageSize,
-      },
-    });
   }
 
   handleRowSelectChange = (selectedRows) => {
@@ -65,199 +55,71 @@ class PageView extends PureComponent {
     })
   }
 
-  handleStandardTableChange = (pagination, filtersArg, sorter) => {
+  handleStandardTableChange = (pagination) => {
     const { dispatch, pageEffectType } = this.props;
-    const { searchFormValues } = this.state;
+    const searchValues = this.getSearchValues();
+    this.doSearch(searchValues, {pageNum: pagination.current, pageSize: pagination.pageSize})
+  };
+
+  handleSearch = () => {
+    const searchValues = this.getSearchValues();
+    this.doSearch(searchValues);
+  }
+
+  doReset = () => {
+    const { defaultPageNum, defaultPageSize } = this.props;
+    this.doSearch({}, {pageNum: defaultPageNum, pageSize: defaultPageSize});
+  };
+
+  doSearch = (searchValues, pagination) => {
+    const {
+      dispatch,
+      pageEffectType,
+      defaultPageNum,
+      defaultPageSize,
+    } = this.props;
+
+    const queryParams = searchValues?searchValues:{};
+    const pageParams = {};
+    if (pagination) {
+      pageParams.pageNum = pagination.pageNum;
+      pageParams.pageSize = pagination.pageSize;
+    } else {
+      const propsPagination = this.props.pageData.pagination;
+      pageParams.pageNum = (propsPagination&&propsPagination.pageNum?propsPagination.pageNum:defaultPageNum);
+      pageParams.pageSize = (propsPagination&&propsPagination.pageSize?propsPagination.pageSize:defaultPageSize);
+    }
 
     const params = {
-      ...searchFormValues,
-      pageNum: pagination.current,
-      pageSize: pagination.pageSize,
+      ...queryParams,
+      ...pageParams
     };
 
     dispatch({
       type: pageEffectType,
       payload: params,
     });
-  };
-
-  handleSearchFormReset = () => {
-    const { form, dispatch, pageEffectType, defaultPageNum, defaultPageSize } = this.props;
-    form.resetFields();
-    this.setState({
-      searchFormValues: {},
-    });
-    dispatch({
-      type: pageEffectType,
-      payload: {
-        pageNum: defaultPageNum,
-        pageSize: defaultPageSize,
-      },
-    });
-  };
-
-  handleSearch = e => {
-    e.preventDefault();
-    this.doSearch();
-  };
-
-  handleShowAdvancedSearchForm = () => {
-    this.setState({
-      showAdvancedSearchForm: !this.state.showAdvancedSearchForm
-    })
   }
-
-  doSearch = () => {
-    const {
-      dispatch,
-      pageEffectType,
-      defaultPageNum,
-      defaultPageSize,
-      form,
-      pageData: { pagination },
-    } = this.props;
-
-    form.validateFields((err, fieldsValue) => {
-      if (err) return;
-
-      const values = {
-        ...fieldsValue,
-        pageNum: defaultPageNum,
-        pageSize: pagination && pagination.pageSize ? pagination.pageSize : defaultPageSize,
-      };
-
-      this.setState({
-        searchFormValues: values,
-      });
-
-      dispatch({
-        type: pageEffectType,
-        payload: values,
-      });
-    });
-  };
 
   doGetSelectedRows = () => {
     return this.state.selectedRows;
   }
 
-  doRenderSimpleSearchForm = () => {
-    const {
-      form: { getFieldDecorator },
-      searchFormItems,
-      searchFormItemLayout,
-      searchFormRowGutter,
-    } = this.props;
-
-    return (
-      <Form onSubmit={this.handleSearch} layout="inline">
-        <Row gutter={searchFormRowGutter}>
-          {searchFormItems.map(item => {
-            return (
-              <Col {...searchFormItemLayout} key={item.name}>
-                <FormItem label={item.label}>
-                  {getFieldDecorator(item.name)(item.component)}
-                </FormItem>
-              </Col>
-            );
-          })}
-          <Col {...searchFormItemLayout} key="action">
-            <span className={styles.submitButtons}>
-              <Button type="primary" htmlType="submit">
-                查询
-              </Button>
-              <Button style={{ marginLeft: 8 }} onClick={this.handleSearchFormReset}>
-                重置
-              </Button>
-            </span>
-          </Col>
-        </Row>
-      </Form>
-    );
-  }
-
-  doRenderAdvancedSearchForm = () => {
-    const {
-      form: { getFieldDecorator },
-      searchFormItems,
-      searchFormItemLayout,
-      searchFormRowGutter,
-      columnSpan
-    } = this.props;
-
-    const { showAdvancedSearchForm } = this.state;
-
-    const totalItems = searchFormItems.length;
-    const totalRows = (totalItems+1)/4 + (totalItems%4>0?1:0);
-
-    let rows = [];
-    let startIndex = 0;
-    let endIndex = 0;
-    for (let i=1; i<=totalRows; i++) {
-      let rowItems = [];
-      if (i === 1) {
-        endIndex = totalItems<(columnSpan-1)?totalItems:(columnSpan-1);
-      } else {
-        endIndex = (i===totalRows?totalItems:startIndex+4);
-      }
-      rowItems = searchFormItems.slice(startIndex, endIndex);
-      rows.push(rowItems);
-      startIndex = endIndex;
-    }
-    
-    return (
-      <Form onSubmit={this.handleSearch} layout="inline">
-        {rows.map((row, rowIndex) => {
-          if (rowIndex === 0) {
-            return (
-              <Row gutter={searchFormRowGutter} key={rowIndex}>
-                {row.map((item) => (
-                  <Col {...searchFormItemLayout} key={item.name}>
-                    <FormItem label={item.label}>
-                      {getFieldDecorator(item.name)(item.component)}
-                    </FormItem>
-                  </Col>
-                ))}
-                <Col {...searchFormItemLayout} key="action">
-                  <span className={styles.submitButtons}>
-                    <Button type="primary" htmlType="submit">
-                      查询
-                    </Button>
-                    <Button style={{ marginLeft: 8 }} onClick={this.handleSearchFormReset}>
-                      重置
-                    </Button>
-                    <Button style={{ marginLeft: 8 }} title="更多" type="link" icon={showAdvancedSearchForm?'up':'down'} onClick={this.handleShowAdvancedSearchForm}>
-                    </Button>
-                  </span>
-                </Col>
-              </Row>
-            )
-          }
-
-          return (
-            <Row gutter={searchFormRowGutter} key={rowIndex} className={showAdvancedSearchForm?'':styles.hidden}>
-              {row.map((item) => (
-                <Col {...searchFormItemLayout} key={item.name}>
-                  <FormItem label={item.label}>
-                    {getFieldDecorator(item.name)(item.component)}
-                  </FormItem>
-                </Col>
-              ))}
-            </Row>
-          )
-        })}
-      </Form>
-    )
-  }
-
   renderSearchForm() {
-    const { searchFormItems, columnSpan } = this.props;
-
-    if (searchFormItems.length < columnSpan) {
-      return this.doRenderSimpleSearchForm();
+    const searchFormProps = {
+      formItems: this.props.searchFormItems,
+      formItemLayout: this.props.searchFormItemLayout,
+      formRowGutter: this.props.searchFormRowGutter,
+      formColumnSpan: this.props.searchFormColumnSpan
     }
-    
-    return this.doRenderAdvancedSearchForm();
+
+    const searchFormMethods = {
+      bindGetSearchValues: (func) => {this.getSearchValues = func;},
+      onSearch: this.doSearch,
+      onReset: this.doReset
+    }
+
+    return <SearchForm {...searchFormProps} {...searchFormMethods}/>
   }
 
   render() {
